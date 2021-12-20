51283359b6b971c6759b2bdf17abbd9b71de98ed
Gombek7 <gombek7@gmail.com>
2021-12-20 17:23:07 +0100

    Ładne przyciski + początek tabliczki powitalnek

--- Kck-projekt-1/Models/GameConfig.cs
+++ Kck-projekt-1/Models/GameConfig.cs
@@ -10,7 +10,7 @@ namespace Kck_projekt_1.Models
         //default 100
         public static int Width { get; } = 100;
         //default 40
-        public static int Height { get; } = 40;
+        public static int Height { get; } = 36;
         public static bool EasyMode { get; set; } = false;
     }
 }
